e35487a3bfab0bf7d1af509c14117e5613742904
myuri001 <63941768+myuri001@users.noreply.github.com>
2022-09-16 18:49:22 +0500

    Удалил ненужное пока. Создал список и в него просто записываются введенные данные. Дальше записывать эти данные в файл

--- main.py
+++ main.py
@@ -1,41 +1,68 @@
 import telebot
+from datetime import date, datetime
 from telebot import types
-
-token = '5392029038:AAFBhcftWuZxyjU6E6waIEID6rdm91DY9fI'
-bot = telebot.TeleBot(token)
-
-tasks = dict()
+import time
+import schedule
 
 
-HELP = '''
-Список доступных команд:
-/add - добавить выученное
-/show - показать всё
-'''
-
-def add_todo(date, task):
-    if date in tasks:
-        tasks[date].append(task)
-    else:
-        tasks[date] = []
-        tasks[date].append(task)
-
-@bot.message_handler(commands=['help'])
-def help(message):
-    bot.send_message(message.chat.id, HELP)
+def set_token():    # выводит токен
+    file_token = 'token.txt'
+    with open(file_token, 'r') as file:  # считывает токен из файла и закрывает его
+        return file.readline()
 
 
-@bot.message_handler(commands=['add'])
-def add(message):
-    command = message.text.split(maxsplit=2)
-    date = command[1]
-    task = command[2]
-    add_todo(date, task)
-    bot.send_message(message.chat.id, f'Задача {task} добавлена на дату {date}')
-    print(tasks)
-@bot.message_handler(commands=['show'])
-def show(message):
-    for i in tasks.items():
-        bot.send_message(message.chat.id, i)
+bot = telebot.TeleBot(set_token())
 
-bot.polling(none_stop=True)
+@bot.message_handler(commands=['start'])
+def start(message):    # Выводит клавиатуру с кнопками
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("/словарь_1")
+    btn2 = types.KeyboardButton("/словарь_2")
+    btn3 = types.KeyboardButton("/словарь_3")
+    btn4 = types.KeyboardButton("/запомнить")
+    markup.add(btn1, btn2, btn3, btn4)
+    bot.send_message(message.chat.id, text="Привет, {0.first_name}!\n Вспоминаем или запоминаем?".format(message.from_user), reply_markup=markup)
+
+@bot.message_handler(commands=['словарь_1'])
+def question_dct1(message):  # при вводе словарь 1 выводит 'Привет', с кнопкой
+    bot.send_message(message.chat.id,
+                     f' Припоминаешь {get_in_dct1()}?',
+                     reply_markup=get_keyboard())
+
+
+def get_keyboard():    # функция кнопки при вводе start
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    button_yes = telebot.types.InlineKeyboardButton('Yes', callback_data='set timer')
+    button_no = telebot.types.InlineKeyboardButton('No', callback_data='set timer')
+    keyboard.add(button_yes, button_no)
+    return keyboard
+
+
+def add_in_dct1(text):  # добавляет введеный пользователем текст в список
+    dct1.append(text)
+
+def get_in_dct1():
+    for i in dct1:
+        return i
+
+@bot.message_handler(commands=['запомнить'])
+def get_text_messages(message):    # Заносит в список и подтверждает занесение
+    add_in_dct1(message.text)
+    bot.send_message(message.chat.id,
+                     f"принял {message.text} и добавл на дату {now}")
+
+    print(dct1)
+
+
+if __name__ == '__main__':
+
+    dct1 = []
+    dct2 = []
+    dct3 = []
+    now = date.today()
+    while True:
+        try:
+            bot.polling()
+        except:
+            print('Что-то сломалось. Перезагрузка')
+
